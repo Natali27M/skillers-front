2f59c7512898586f85ae7bdcea3ecc1454ebc6f6
MVPavlyk <grandbityyy67@gmail.com>
2022-10-14 11:44:01 +0300

    first commit

---
+++ src/pages/CompilerPage/CompilerPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './CompilerPage.module.css'
+
+const CompilerPage = () => {
+    return (
+        <div>
+            CompilerPage
+        </div>
+    );
+};
+
+export {CompilerPage};
