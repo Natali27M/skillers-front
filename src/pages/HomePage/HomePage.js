b9b180523e67f30c80ef802d9d1db9b4750a6e31
Маліцька Наталя <91392018+Natali27M@users.noreply.github.com>
2022-11-03 14:01:58 +0200

    Change style

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-
 import css from './HomePage.module.css';
-import {LeaderBord, TechList} from '../../components';
+import logo from '../../images/header/SKILLERS.svg';
+import {LeaderBord, TechList, Banner} from '../../components';
+import {Link} from 'react-router-dom';
 
 
 const HomePage = () => {
     const {EN} = useSelector(state => state['languageReducers']);
-    // const {user} = useSelector(state => state['userReducers']);
+    const {user} = useSelector(state => state['userReducers']);
 
     return (
         <>
@@ -20,9 +21,9 @@ const HomePage = () => {
                     {EN ? 'Platform for testing your IT skills' :
                         'Платформа для перевірки твоїх IT навичок'}
                 </div>
-                {/*<Link to={user ? '/user' : '/registration'} className={css.register__btn}>*/}
-                {/*    {user ? (EN ? 'To my profile' : 'На мій профіль') : (EN ? 'Register now' : 'Зареєструватися')}*/}
-                {/*</Link>*/}
+                <Link to={user ? '/user' : '/registration'} className={css.register__btn}>
+                    {user ? (EN ? 'To my profile' : 'На мій профіль') : (EN ? 'Register now' : 'Зареєструватися')}
+                </Link>
             </div>
             <TechList/>
             <LeaderBord/>
@@ -32,3 +33,39 @@ const HomePage = () => {
 };
 
 export {HomePage};
+
+
+// import React from 'react';
+// import {useSelector} from 'react-redux';
+//
+// import css from './HomePage.module.css';
+// import {LeaderBord, TechList} from '../../components';
+//
+//
+// const HomePage = () => {
+//     const {EN} = useSelector(state => state['languageReducers']);
+//     // const {user} = useSelector(state => state['userReducers']);
+//
+//     return (
+//         <>
+//             <div className={css.home__page}>
+//                 {/*<img className={css.home__logo} src={logo} alt="logo"/>*/}
+//                 <h1 className={css.main__title}>
+//                     SKILLIANT
+//                 </h1>
+//                 <div className={css.home__description}>
+//                     {EN ? 'Platform for testing your IT skills' :
+//                         'Платформа для перевірки твоїх IT навичок'}
+//                 </div>
+//                 {/*<Link to={user ? '/user' : '/registration'} className={css.register__btn}>*/}
+//                 {/*    {user ? (EN ? 'To my profile' : 'На мій профіль') : (EN ? 'Register now' : 'Зареєструватися')}*/}
+//                 {/*</Link>*/}
+//             </div>
+//             <TechList/>
+//             <LeaderBord/>
+//             {/*<Banner/>*/}
+//         </>
+//     );
+// };
+//
+// export {HomePage};
