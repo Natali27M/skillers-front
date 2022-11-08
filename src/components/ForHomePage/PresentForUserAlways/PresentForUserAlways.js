5c67078e5fd0a253ca9c1a53a9416582951921d7
Маліцька Наталя <91392018+Natali27M@users.noreply.github.com>
2022-11-08 15:01:39 +0200

    I made a div for the present for top 10 users, siled it and adapted it

--- src/components/ForHomePage/PresentForUserAlways/PresentForUserAlways.js
+++ src/components/ForHomePage/PresentForUserAlways/PresentForUserAlways.js
@@ -3,13 +3,22 @@ import React from 'react';
 import css from './PresentForUserAlways.module.css';
 
 const PresentForUserAlways = () => {
+
     return (
         <div className={css.presentBox}>
-            <div className={css.text}>
-                <h2> <span className={css.name}>
-                    Skilliant</span> платить <span>100$</span> для перших <span>10</span> користувачів у таблиці лідерів
-                </h2>
-            </div>
+
+                <div className={css.overlay}>
+
+                    <div className={css.text}>
+                        <h2>
+                            <span className={css.name}>Skilliant</span> платить <span>100$</span> для <br/>
+                            перших <span>10</span> користувачів <br/>
+                            у таблиці лідерів
+                        </h2>
+                    </div>
+
+                </div>
+
         </div>
     );
 };
