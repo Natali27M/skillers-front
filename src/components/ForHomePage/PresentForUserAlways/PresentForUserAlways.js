45128151ba380d82024091b0c9409ba5a41dd496
Маліцька Наталя <91392018+Natali27M@users.noreply.github.com>
2022-11-09 21:34:38 +0200

    Change baner

--- src/components/ForHomePage/PresentForUserAlways/PresentForUserAlways.js
+++ src/components/ForHomePage/PresentForUserAlways/PresentForUserAlways.js
@@ -10,7 +10,19 @@ const PresentForUserAlways = () => {
         <div className={css.presentBox}>
             <div className={css.banner__angle}></div>
             <div className={css.banner__content}>
-
+                {EN ?
+                    <div>
+                        <p> <span className={css.span}>Skilliant</span> pays <span className={css.span}>100$</span></p>
+                        <p>for the first <span className={css.span}>10</span> users</p>
+                        <p>in the leaderbord</p>
+                    </div>
+                    :
+                    <div>
+                        <p> <span className={css.span}>Skilliant</span> платить <span className={css.span}>100$</span></p>
+                        <p>для перших <span className={css.span}>10</span> користувачів</p>
+                        <p>у таблиці лідерів</p>
+                    </div>
+                }
             </div>
         </div>
     );
