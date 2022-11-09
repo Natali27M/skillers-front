4ef2dacdbe6ed939dae88578fe624ba3a0c68c2b
MVPavlyk <grandbityyy67@gmail.com>
2022-11-09 18:39:43 +0200

    css fix

--- src/components/ForHomePage/PresentForUser/PresentForUser.js
+++ src/components/ForHomePage/PresentForUser/PresentForUser.js
@@ -6,14 +6,13 @@ const PresentForUser = () => {
     const {EN} = useSelector(state => state['languageReducers']);
 
     const setLocalStorage = () => {
-        localStorage.setItem('present', 'true');
-        window.location.reload();
+        /*localStorage.setItem('present', 'true');
+        window.location.reload();*/
     }
 
     return (
         <div className={css.present__wrap}>
             <div className={css.present__block}>
-
                 <div className={css.crossBox}>
                     <div className={css.cross} onClick={() => setLocalStorage()}></div>
                 </div>
@@ -38,11 +37,6 @@ const PresentForUser = () => {
                     </p>
 
                 </div>
-                {/*<Link to={'/registration'} className={rootCSS.default__button}>*/}
-                {/*    {EN ? 'Register now' : 'Зареєструватись зараз'}*/}
-                {/*</Link>*/}
-                {/*<div onClick={() => setModalOpen(false)}*/}
-                {/*     className={css.no__thanks}>{EN ? 'No, thanks' : 'Ні, дякую'}</div>*/}
             </div>
         </div>
     );
@@ -50,37 +44,3 @@ const PresentForUser = () => {
 
 export {PresentForUser};
 
-// import React from 'react';
-//
-// import css from './PresentForUser.module.css';
-// import {Link} from 'react-router-dom';
-// import rootCSS from '../../../styles/root.module.css';
-// import {useSelector} from 'react-redux';
-//
-// const PresentForUser = ({type}) => {
-//     // const {EN} = useSelector(state => state['languageReducers']);
-//
-//     return (
-//         <div className={css.modal__wrap}>
-//             <div className={css.modal__block}>
-//                 <div className={css.modal__text}>
-//                     Hello
-//                 {/*    {EN ? 'You passed the test successfully ' +*/}
-//                 {/*        'but, unfortunately, you won\'t be able to ' +*/}
-//                 {/*        'save the result and ' +*/}
-//                 {/*        'compete with others ' +*/}
-//                 {/*        'by users. We advise you to register to save your result next time' : 'Ви успішно пройшли тест, але, на жаль, не зможете ' +*/}
-//                 {/*        'зберегти результат та позмагатися з іншими користувачами. ' +*/}
-//                 {/*        'Радимо зареєструватись, щоб наступного разу зберегти свій результат'}*/}
-//                 </div>
-//                 {/*<Link to={'/registration'} className={rootCSS.default__button}>*/}
-//                 {/*    {EN ? 'Register now' : 'Зареєструватись зараз'}*/}
-//                 {/*</Link>*/}
-//                 {/*<div onClick={() => setModalOpen(false)}*/}
-//                 {/*     className={css.no__thanks}>{EN ? 'No, thanks' : 'Ні, дякую'}</div>*/}
-//             </div>
-//         </div>
-//     );
-// };
-//
-// export default PresentForUser;
