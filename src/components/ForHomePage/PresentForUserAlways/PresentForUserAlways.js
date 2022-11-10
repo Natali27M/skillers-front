371192ed05da38d82066d35d4816a490fe80945d
Маліцька Наталя <91392018+Natali27M@users.noreply.github.com>
2022-11-10 14:29:32 +0200

    Change style description login user

--- src/components/ForHomePage/PresentForUserAlways/PresentForUserAlways.js
+++ src/components/ForHomePage/PresentForUserAlways/PresentForUserAlways.js
@@ -11,17 +11,33 @@ const PresentForUserAlways = () => {
             <div className={css.banner__angle}></div>
             <div className={css.banner__content}>
                 {EN ?
-                        <p className={css.banner__text}> <span className={css.banner__name}>Skilliant </span>
-                            pays <span className={css.banner__underline}>100$</span> <br/>
-                            for the first <span className={css.banner__underline}>10</span> users <br/>
-                            in the leaderbord
-                        </p>
+                    <p className={css.banner__text}><span className={css.banner__name}>Skilliant </span>
+                        will split <span className={css.banner__underline}>100$</span><br/>
+                        between by the first <span className={css.banner__underline}>10</span><br/>
+                        users in the table
+                    </p>
                     :
-                        <p className={css.banner__text}> <span className={css.banner__name}>Skilliant </span>
-                            платить <span className={css.banner__underline}>100$</span> для<br/>
-                            перших <span className={css.banner__underline}>10</span> користувачів <br/>
-                            у таблиці лідерів
-                        </p>
+                    <p className={css.banner__text}> <span className={css.banner__name}>Skilliant </span>
+                        розділить <span className={css.banner__underline}>100$</span> між<br/>
+                        першими <span className={css.banner__underline}>10</span> користувачами <br/>
+                        у таблиці лідерів
+                    </p>
+
+                    // <p className={css.banner__text}><span className={css.banner__name}>Skilliant </span>
+                    // pays <span className={css.banner__underline}>100$</span> <br/>
+                    // for the first <span className={css.banner__underline}>10</span> users <br/>
+                    // in the leaderbord
+                    // </p>
+
+                    // <p className={css.banner__text}>
+                    //     <span className={css.banner__underline}>10</span> користувачів<span className={css.banner__name}>Skilliant </span>
+                    //     розділять між собою<span className={css.banner__underline}>100$</span>
+                    // </p>
+                    // <p className={css.banner__text}> <span className={css.banner__name}>Skilliant </span>
+                    //     платить <span className={css.banner__underline}>100$</span> для<br/>
+                    //     перших <span className={css.banner__underline}>10</span> користувачів <br/>
+                    //     у таблиці лідерів
+                    // </p>
                 }
             </div>
         </div>
