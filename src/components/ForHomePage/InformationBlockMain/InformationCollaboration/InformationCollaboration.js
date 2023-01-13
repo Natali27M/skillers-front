1ee6ccf8552ebe43817916bf7a1240e37f8b0797
Маліцька Наталя <91392018+Natali27M@users.noreply.github.com>
2023-01-13 21:41:26 +0200

    Add information telegram

--- src/components/ForHomePage/InformationBlockMain/InformationCollaboration/InformationCollaboration.js
+++ src/components/ForHomePage/InformationBlockMain/InformationCollaboration/InformationCollaboration.js
@@ -5,9 +5,26 @@ import cssThis from './InformationCollaboration.module.css';
 import collaboration from '../../../../images/information/collaboration.png';
 
 const InformationCollaboration = () => {
+    // const observer = new IntersectionObserver(entries => {
+    //     entries.forEach(entry => {
+    //         const square = entry.target.querySelector('.testing__img');
+    //
+    //         if (entry.isIntersecting) {
+    //             square.classList.add('testing__img_active');
+    //             return; // if we added the class, exit the function
+    //         }
+    //
+    //         // We're not intersecting, so remove the class!
+    //         square.classList.remove('testing__animation');
+    //     });
+    // });
+    //
+    // observer.observe(document.querySelector('.square-wrapper'));
     return (
         <div className={css.testing__main}>
+            {/*<div className={cssThis.testing__animation}>*/}
             <img src={collaboration} alt="testing" className={cssThis.testing__img}/>
+            {/*</div>*/}
 
             <div className={css.testing__text_box}>
                 <h4 className={css.testing__header}>Collaborative programming</h4>
@@ -19,6 +36,23 @@ const InformationCollaboration = () => {
                     coding from SKILLIANT.
                 </p>
             </div>
+            {/*<script>*/}
+            {/*     const observer = new IntersectionObserver(entries => {*/}
+            {/*         entries.forEach(entry => {*/}
+            {/*             const square = entry.target.querySelector('.testing__img');*/}
+
+            {/*             if (entry.isIntersecting) {*/}
+            {/*                 square.classList.add('testing__img_active');*/}
+            {/*                 return; // if we added the class, exit the function*/}
+            {/*             }*/}
+
+            {/*             // We're not intersecting, so remove the class!*/}
+            {/*             square.classList.remove('testing__img_active');*/}
+            {/*         })*/}
+            {/*     });*/}
+
+            {/*     observer.observe(document.querySelector('.square-wrapper'));*/}
+            {/*</script>*/}
         </div>
     );
 };
