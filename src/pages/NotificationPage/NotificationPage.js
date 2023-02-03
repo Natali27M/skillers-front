5a0d42069c347d660fe386feaab1da5f83325059
MVPavlyk <grandbityyy67@gmail.com>
2023-02-03 09:26:45 +0200

    optimisation

--- src/pages/NotificationPage/NotificationPage.js
+++ src/pages/NotificationPage/NotificationPage.js
@@ -9,6 +9,7 @@ import css from './NotificationPage.module.css';
 const NotificationPage = () => {
     const {user} = useSelector(state => state['userReducers']);
     const {notifications} = useSelector(state => state['notificationReducers']);
+    const {EN} = useSelector(state => state['languageReducers']);
     const [pageNumber, setPageNumber] = useState(1);
     const dispatch = useDispatch();
     const userId = user?.id;
@@ -24,13 +25,25 @@ const NotificationPage = () => {
         <div className={cssPosts.posts__main_box}>
             {/*<Information/>*/}
             <div className={css.notifications__main}>
-                <div className={css.notifications__main_box}>
-                    {notifications?.data && notifications.data.map(value => <Notification key={value.id} notification={value}/>)}
-                </div>
-                <PaginationSmall
-                    pageNumber={notifications?.meta?.pagination.page}
-                    setPageNumber={setPageNumber}
-                    pageCount={notifications?.meta?.pagination.pageCount}/>
+                {notifications?.data?.length ?
+                    <>
+                        <div className={css.notifications__main_box}>
+                            {notifications?.data && notifications.data.map(value => <Notification key={value.id}
+                                                                                                  notification={value}/>)}
+                        </div>
+                    </>
+                    :
+                    <div
+                        className={css.no__notification}>{EN ? 'You have no notifications' : 'У вас немає сповіщень'}</div>
+                }
+                {!!notifications?.data?.length &&
+                    <PaginationSmall
+                        pageNumber={notifications?.meta?.pagination.page}
+                        setPageNumber={setPageNumber}
+                        pageCount={notifications?.meta?.pagination.pageCount}
+                    />
+                }
+
             </div>
 
         </div>
